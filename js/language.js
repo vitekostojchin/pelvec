170bb0c9252f0559108a042fb83ee3ffae39bb2f
vitekostojchin <vkostojcinoski@gmail.com>
2025-05-01 01:38:01 +0200

    language update

---
+++ js/language.js
@@ -0,0 +1,85 @@
+var languageData = {
+    en: {
+      navabout: "About",
+      navmenu: "Menu",
+      navgallery: "Gallery",
+      navcontact: "Contact",
+      abouttitle: "Our Restaurant",
+      abouttext1: "Informations about restaurant, when is established, what we offer...",
+      aboutsubtitle: "Something else or not",
+      abouttext2: "Some more informations about restaurant, what are our special dishesh etc...",
+      menutitle: "Our Menu",
+      menutext: "Tap below to view or download our full menu:",
+      menutext2: "Click to view full menu",
+      gallerytitle: "Gallery",
+      gallerytext: "See pictures from our dishes",
+      all: "All",
+      starters: "Starters",
+      maindishes: "Main Dishes",
+      dessert: "Dessert",
+      reservationtitle: "Want to make a reservation? Call <strong><a href='tel:+38970870292'>070 870 292</a></strong>",
+      reservationphone: "070 870 292",
+      addresstitle: "Address",
+      street: "Vevchani 6335",
+      country: "Republic of North Macedonia",
+      workinghourstitle: "Working Hours",
+      weekdayhours: "Tue-Thurs: 12:00 AM - 12:00 PM",
+      weekendhours: "Fri-Sun: 11:00 AM - 01:00 AM",
+      contacttitle: "Contact Info",
+      contactphone: "Phone: 070 870 292",
+      contactemail: "Email: info@pelvec.com",
+    },
+    mk: {
+        navabout: "За нас",
+        navmenu: "Мени",
+        navgallery: "Галерија",
+        navcontact: "Контакт",
+        abouttitle: "Нашиот Ресторан",
+        abouttext1: "Информации за ресторанот, кога е основан, што нудиме...",
+        aboutsubtitle: "Нешто друго или не",
+        abouttext2: "Некои дополнителни информации за ресторанот, кои се нашите специјални јадења и сл...",
+        menutitle: "Нашето Мени",
+        menutext: "Кликнете подолу за да го погледнете или преземете нашето целосно мени:",
+        menutext2: "Кликнете за да го видите целото мени",
+        gallerytitle: "Галерија",
+        gallerytext: "Погледнете фотографии од нашите јадења",
+        all: "Сите",
+        starters: "Предјадења",
+        maindishes: "Главни Јадења",
+        dessert: "Десерти",
+        reservationtitle: "Сакате да направите резервација? Повикајте <strong><a href='tel:+38970870292'>070 870 292</a></strong>",
+        reservationphone: "070 870 292",
+        addresstitle: "Адреса",
+        street: "Вевчани 6335",
+        country: "Република Северна Македонија",
+        workinghourstitle: "Работно време",
+        weekdayhours: "Втор-Четв: 12:00 AM - 12:00 PM",
+        weekendhours: "Пет-Нед: 11:00 AM - 01:00 AM",
+        contacttitle: "Контакт Инфо",
+        contactphone: "Телефон: 070 870 292",
+        contactemail: "Емаил: info@pelvec.com"
+      }
+};
+
+// Load default language (English)
+var currentLanguage = "en";
+
+// Function to change language
+function setLanguage() {
+    var lang = document.getElementById("language-selector").value;
+    currentLanguage = lang;
+    loadLanguage();
+}
+
+// Function to load the language
+function loadLanguage() {
+    var keys = document.querySelectorAll("[data-key]");
+    keys.forEach(function (key) {
+        var keyName = key.getAttribute("data-key");
+        if (languageData[currentLanguage][keyName]) {
+            key.innerHTML = languageData[currentLanguage][keyName];
+        }
+    });
+}
+
+loadLanguage();
